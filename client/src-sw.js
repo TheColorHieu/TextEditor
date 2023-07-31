a1a5695e333dc59c9eebed46e4c602be1413fc9a
Hieu Tran <hieuthanhtran98@yahoo.com>
2023-07-31 09:10:53 -0700

    added our comments to the src-sws

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,15 +27,24 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
+//this is the code for caching the assets
+//this allows us to define custom caching strategies for different types of assets
 registerRoute(
+  //here we are determining the type of assets to cache
   ({ request }) => request.destination === 'script' || request.destination === 'style',
+  //here we are defining the caching strategy
   new CacheFirst({
+    //here we are defining the name of the cache
     cacheName: 'asset-cache',
+    //here we are defining the plugins
     plugins: [
+      //here we are defining the cacheable response plugin
       new CacheableResponsePlugin({
         statuses: [0, 200],
       }),
+      //here we are defining the expiration plugin
       new ExpirationPlugin({
+        //here we are defining the maximum age of the cache
         maxAgeSeconds: 30 * 24 * 60 * 60,
       }),
     ],

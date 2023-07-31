d60be99d5af9342ddb24c615765e7e34a58e591d
Hieu Tran <hieuthanhtran98@yahoo.com>
2023-07-31 08:52:39 -0700

    here we added some comments in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,32 +13,46 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
+//this function is responsible for saving the data to the database
 export const putDb = async (content) => {
-  console.error('putDb not implemented');
-const jateDb = openDB('jate', 1);
-const tx = jateDb.transaction('jate', 'readwrite');
-const store = tx.objectStore('jate');
-const request = store.put({ id:1, value: content});
-console.log(request); 
-
-const result = await request;
-console.log("data was successfully  added to the database",result);
-return result;
+  console.log("Put to the database");
+//open the database
+  const jateDb = await openDB("jate", 1);
+  //start a transaction in readwrite mode to access the database and store the data in the object store
+  const tx = jateDb.transaction("jate", "readwrite");
+  //access the object store
+  const store = tx.objectStore("jate");
+  //put the data in the object store
+  const request = store.put({ id: 1, value: content });
+  //log the request object that represents the request to the database
+  console.log(request);
+  //wait for the request to complete
+  const result = await request;
+  //log the result
+  console.log("data saved to the database", result);
+  //return the result
+  return result;
 };
 
 
 
 // TODO: Add logic for a method that gets all the content from the database
+//this function is responsible for getting the data from the database
 export const getDb = async () => {
-  console.error('getDb not implemented');
-const jateDb = await openDB('jate', 1);
-const tx = jateDb.transaction('jate', 'readonly');
-const store = tx.objectStore('jate');
-const request = store.getAll();
-const result = await request;
-console.log("data was successfully  retrieved from the database",result);
-return result;
+  console.log("GET from the database");
+  //here we are opening the database 
+  const jateDb = await openDB("jate", 1);
+  //start a transaction in readonly mode to access the database and store the data in the object store
+  const tx = jateDb.transaction("jate", "readonly");
+  //access the object store
+  const store = tx.objectStore("jate");
+  //here we are fetching all operation to complete the request
+  const request = store.getAll();
+  //log the request object that represents the request to the database
+  const result = await request;
+  //log the result of the request
+  console.log("result.value", result);
+  //return the result
+  return result?.value;
 };
-
-
 initdb();
